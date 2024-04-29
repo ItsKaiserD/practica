a19fa7b11c299ae0daf04ea3751a0162c93c0fd7
ItsKaiserD <lu.perez.cabrera01@gmail.com>
2024-04-29 18:59:05 -0400

    Prototipo Final
    La interfaz funciona bien, todavía existe espacio de mejora, pero para una primera impresión o algún prototipo puede servir. En los siguientes días me encargaré de juntar todos los assets ajenos al código (imágenes, colores que usaré, etc.) antes de ponerme a trabajar en la GUI definitiva.

--- GUI_Prototipo_Pruebas.py
+++ GUI_Prototipo_Pruebas.py
@@ -5,28 +5,35 @@ from PIL import Image, ImageTk
 
 # Creamos la ventana principal
 window = Tk()
-#Se le asigna un tamaño inicial 
-window.geometry("600x400")
-#Se le pone un título a la ventana
+#Título de la Ventana
 window.title("PVV INGENIERIA")
 #Configuramos para que el tamaño sea reajustable o no
 window.resizable(width="true", height="true")
 
+#Definimos una función para cerrar la interfaz
 def close():
     window.destroy()
 
+#Abrimos una imagen que se usara como logo en la esquina de la pantalla, para luego 
+#reajustar su tamaño más que nada para que quepa bien en la ventana. 
 portada = Image.open("logo_prueba.png")
 prtd_resize = portada.resize((250,100))
 img_portada = ImageTk.PhotoImage(prtd_resize)
 
+#Abrimos otra imagen que voy a usar como placeholder para los diversos módulos, en los siguientes días 
+#estaré trabajando en buscar o hacer las fotos necesarias.
 perfil_prueba = Image.open("perfil.png")
 prfl_resize = perfil_prueba.resize((75,75))
 prfil = ImageTk.PhotoImage(prfl_resize)
 
-
-#Widgets (ESTA ES LA MANERA MÁS CAVERNICOLA QUE HAY, PERO ES LA UNICA QUE ME HA RESULTADO XDDDDDDDDD)
-img_label = Label(window, image=img_portada)
+#ESTA ES POSIBLEMENTE LA FORMA MÁS BRUTA DE HACER ESTE PROYECTO, PERO DE QUE FUNCIONA, FUNCIONA
+#Widgets Principales
+img_label = Label(window, image=img_portada, background="yellow")
 exit_btn = Button(window, text="Salir de la Aplicación", command=close)
+exit_btn.config(background="yellow", activebackground="blue")
+midlbl1 = Label(window, background="yellow")
+midlbl2 = Label(window, background="yellow")
+
 #Título de los Módulos
 titulo1 = Label(window, text="Finanzas")
 titulo1.config(font=("Arial",15))
@@ -44,7 +51,8 @@ titulo7 = Label(window, text="Capcitaciones")
 titulo7.config(font=("Arial",15))
 titulo8 = Label(window, text="Otros")
 titulo8.config(font=("Arial",15))
-#Imagén de los Módulos
+
+#Imagenes de los Módulos
 img1 = Label(window, image=prfil)
 img2 = Label(window, image=prfil)
 img3 = Label(window, image=prfil)
@@ -53,7 +61,8 @@ img5 = Label(window, image=prfil)
 img6 = Label(window, image=prfil)
 img7 = Label(window, image=prfil)
 img8 = Label(window, image=prfil)
-#Texto de los Módulos
+
+#Texto Descriptivo de los Módulos de los Módulos
 txt1 = Label(window, text="WHEN YOU SEE MY FACE,\n HOPE IT GIVES YOU HELL,\n HOPE IT GIVES YOU HELL")
 txt2 = Label(window, text="BUCKLE UP,\n RUB A DUB DUB,\n YEAH YEAH,\n TOO LATE MY BABY")
 txt3 = Label(window, text="WHATS THE WORST THAT I CAN SAY\n THINGS ARE BETTER IF I STAY\n SO LONG AND GOODNIGHT")
@@ -62,17 +71,18 @@ txt5 = Label(window, text="COME, BREAK ME DOWN\n BURY ME, BURY ME\n I AM FINISHE
 txt6 = Label(window, text="IVE BECOME SO NUMB\n I CANT FELL YOU THERE\n BECOME SO TIRED\n SO MUCH MORE AWARE")
 txt7 = Label(window, text="IT JUST TAKE SOME TIME\n LITTLE GIRL YOU ARE\n IN THE MIDDLE OF THE RIDE")
 txt8 = Label(window, text="IF I GO CRAZY\n THEN WILL YOU STILL\n CALL ME SUPERMAN?")
-#Botón de los Módulos
-btn1 = Button(window, text="Ingresar")
-btn2 = Button(window, text="Ingresar")
-btn3 = Button(window, text="Ingresar")
-btn4 = Button(window, text="Ingresar")
-btn5 = Button(window, text="Ingresar")
-btn6 = Button(window, text="Ingresar")
-btn7 = Button(window, text="Ingresar")
-btn8 = Button(window, text="Ingresar")
 
-#Configuración de las Columnas y Filas 
+#Botón de los Módulos
+btn1 = Button(window, text="Ingresar", background="yellow", activebackground="blue")
+btn2 = Button(window, text="Ingresar", background="yellow", activebackground="blue")
+btn3 = Button(window, text="Ingresar", background="yellow", activebackground="blue")
+btn4 = Button(window, text="Ingresar", background="yellow", activebackground="blue")
+btn5 = Button(window, text="Ingresar", background="yellow", activebackground="blue")
+btn6 = Button(window, text="Ingresar", background="yellow", activebackground="blue")
+btn7 = Button(window, text="Ingresar", background="yellow", activebackground="blue")
+btn8 = Button(window, text="Ingresar", background="yellow", activebackground="blue")
+
+#Configuración de las Columnas y Filas de la ventana 
 window.columnconfigure((0,1,2,3), weight=200)
 window.rowconfigure(0, weight= 200)
 window.rowconfigure(1, weight= 200)
@@ -84,9 +94,11 @@ window.rowconfigure(6, weight= 200)
 window.rowconfigure(7, weight= 200)
 window.rowconfigure(8, weight= 200)
 
-#Insertar los Widgets
-img_label.grid(column=0, row=0)
-exit_btn.grid(column=3, row=0)
+#Inserción de los Widgets 
+img_label.grid(column=0, row=0, sticky="nsew")
+midlbl1.grid(column=1, row=0, sticky="nsew")
+midlbl2.grid(column=2, row=0, sticky="nsew")
+exit_btn.grid(column=3, row=0, sticky="nsew")
 #Títulos
 titulo1.grid(row=1, column=0)
 titulo2.grid(row=1, column=1)
